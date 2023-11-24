761b26b1d8bf1407bd4d75eeb7443ec1c7a78f8c
Luis Felipe Evilla <luisevilla588@gmail.com>
2023-11-23 21:45:06 -0500

    Add name parameter to charactersBySpecies query

--- src/app/api/characters/route.ts
+++ src/app/api/characters/route.ts
@@ -4,12 +4,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const page = req.nextUrl.searchParams.get('page') || 1;
+  const name = req.nextUrl.searchParams.get('name') || '';
 
   try {
     const response = await axios.post(API_BASE_URL, {
       query: `
             query {
-                charactersBySpecies(species: "Human", page: ${page}, limit: 5) {
+                charactersBySpecies(species: "Human", page: ${page}, limit: 5, name: "${name}") {
                   info {
                     count
                     pages
